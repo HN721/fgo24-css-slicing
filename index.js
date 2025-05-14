89b82630e5f5a245cb242b67e215157c4379f9f6
HN721 <hosea1422@gmail.com>
2025-05-14 08:56:46 +0700

    feat:update transaction

--- index.js
+++ index.js
@@ -1,3 +1,106 @@
+const people = [
+  {
+    name: "Ghaluh 1",
+    image: "https://randomuser.me/api/portraits/men/1.jpg",
+    phone: "(239) 555–0101",
+  },
+  {
+    name: "Albert Flores",
+    image: "https://randomuser.me/api/portraits/men/2.jpg",
+    phone: "(239) 555–0102",
+  },
+  {
+    name: "Bessie Cooper",
+    image: "https://randomuser.me/api/portraits/women/3.jpg",
+    phone: "(239) 555–0103",
+  },
+  {
+    name: "Jacob Jones",
+    image: "https://randomuser.me/api/portraits/men/4.jpg",
+    phone: "(239) 555–0104",
+  },
+  {
+    name: "Leslie Alexander",
+    image: "https://randomuser.me/api/portraits/women/5.jpg",
+    phone: "(239) 555–0105",
+  },
+  {
+    name: "Cody Fisher",
+    image: "https://randomuser.me/api/portraits/men/6.jpg",
+    phone: "(239) 555–0106",
+  },
+  {
+    name: "Jenny Wilson",
+    image: "https://randomuser.me/api/portraits/women/7.jpg",
+    phone: "(239) 555–0107",
+  },
+  {
+    name: "Dianne Russell",
+    image: "https://randomuser.me/api/portraits/women/8.jpg",
+    phone: "(239) 555–0108",
+  },
+  {
+    name: "Ronald Richards",
+    image: "https://randomuser.me/api/portraits/men/9.jpg",
+    phone: "(239) 555–0109",
+  },
+  {
+    name: "Arlene McCoy",
+    image: "https://randomuser.me/api/portraits/women/10.jpg",
+    phone: "(239) 555–0110",
+  },
+  {
+    name: "Savannah Nguyen",
+    image: "https://randomuser.me/api/portraits/women/11.jpg",
+    phone: "(239) 555–0111",
+  },
+  {
+    name: "Wade Warren",
+    image: "https://randomuser.me/api/portraits/men/12.jpg",
+    phone: "(239) 555–0112",
+  },
+  {
+    name: "Jane Cooper",
+    image: "https://randomuser.me/api/portraits/women/13.jpg",
+    phone: "(239) 555–0113",
+  },
+  {
+    name: "Eleanor Pena",
+    image: "https://randomuser.me/api/portraits/women/14.jpg",
+    phone: "(239) 555–0114",
+  },
+  {
+    name: "Floyd Miles",
+    image: "https://randomuser.me/api/portraits/men/15.jpg",
+    phone: "(239) 555–0115",
+  },
+  {
+    name: "Annette Black",
+    image: "https://randomuser.me/api/portraits/women/16.jpg",
+    phone: "(239) 555–0116",
+  },
+  {
+    name: "Esther Howard",
+    image: "https://randomuser.me/api/portraits/women/17.jpg",
+    phone: "(239) 555–0117",
+  },
+  {
+    name: "Guy Hawkins",
+    image: "https://randomuser.me/api/portraits/men/18.jpg",
+    phone: "(239) 555–0118",
+  },
+  {
+    name: "Marvin McKinney",
+    image: "https://randomuser.me/api/portraits/men/19.jpg",
+    phone: "(239) 555–0119",
+  },
+  {
+    name: "Kristin Watson",
+    image: "https://randomuser.me/api/portraits/women/20.jpg",
+    phone: "(239) 555–0120",
+  },
+];
+
 const container = document.querySelector(".table-trans");
 const prevBtn = document.getElementById("prev-btn");
 const nextBtn = document.getElementById("next-btn");
@@ -6,29 +109,9 @@ const currentRange = document.getElementById("current-range");
 const totalEntries = document.getElementById("total-entries");
 const searchInput = document.querySelector("#search");
 
-const url =
-  "https://raw.githubusercontent.com/HN721/fgo24-source-ewallet/refs/heads/main/source-code.txt";
-
-let people = [];
-let filteredPeople = [];
-
 const itemsPerPage = 5;
 let currentPage = 1;
-
-async function fetchPeople() {
-  try {
-    const response = await fetch(url);
-    if (!response.ok) {
-      throw new Error("Gagal fetch data");
-    }
-    const textData = await response.json();
-    people = textData;
-    filteredPeople = [...people];
-    displayPeople(currentPage, filteredPeople);
-  } catch (error) {
-    console.error("Error saat mengambil data:", error);
-  }
-}
+let filteredPeople = [...people];
 
 function displayPeople(page, peopleList) {
   container.innerHTML = "";
@@ -39,12 +122,11 @@ function displayPeople(page, peopleList) {
   paginatedPeople.forEach((item) => {
     const listTrans = document.createElement("a");
     listTrans.classList.add("list-trans");
-    listTrans.href = `/src/Dashboard/detail.html?"name"=${encodeURIComponent(
+    listTrans.href = `/src/Dashboard/detail.html?name=${encodeURIComponent(
       item.name
-    )}&"phone"=${encodeURIComponent(item.phone)}&"image"=${encodeURIComponent(
+    )}&phone=${encodeURIComponent(item.phone)}&image=${encodeURIComponent(
       item.image
     )}`;
-
     const img = document.createElement("img");
     img.src = item.image;
     img.alt = item.name;
@@ -121,4 +203,4 @@ nextBtn.addEventListener("click", () => {
 
 searchInput.addEventListener("input", searchData);
 
-fetchPeople();
+displayPeople(currentPage, filteredPeople);
